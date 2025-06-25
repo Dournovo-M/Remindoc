c55d1ede41a42f00d5ec931f5d286dee4cb09d08
Dournovo - M <m-dournovo@workmail.com>
2025-06-25 19:22:42 +0200

    new

--- link-db.php
+++ link-db.php
@@ -1,15 +1,18 @@
 <?php
-// Paramètres de connexion à la base de données
-$host = 'mistyrose-anteater-380389.hostingersite.com';       // ou IP serveur MySQL
-$dbname = 'u858625601_remindoc'; // Remplace par le nom de ta base
-$user = 'u858625601_remindoc';            // utilisateur MySQL
-$password = '20DeC2007@#';            // mot de passe MySQL
+$host = '127.0.0.1:3306';
+$dbname = 'u858625601_remindoc';
+$user = 'u858625601_root';
+$password = '20DeC2007@#';
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password);
-    // Mode erreur en exception
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_TIMEOUT => 5, // timeout 5 sec
+    ]);
 } catch (PDOException $e) {
-    die("Erreur de connexion à la base de données : " . $e->getMessage());
+    echo "Erreur de connexion à la base de données : " . $e->getMessage();
+    exit;
 }
+
+echo "Connexion réussie !";
 ?>
